306ea8a0f0286671b2c4263fdf6813895c39f54d
Mateusz Rytwiński <mateuszrytwinski@onet.pl>
2021-05-06 21:32:01 +0200

    added news and sales

---
+++ Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using MVCShop.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCShop.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        public async Task<ActionResult> Index()
+        {
+            var dateInThePast = DateTime.Now.AddDays(-7);
+            var products = db.Products.Where(p => p.Deleted == false && p.Visible == true && p.Date > dateInThePast).Include(p => p.Category);
+            return View(await products.ToListAsync());
+        }
+    }
+}
---
+++ Controllers/SalesController.cs
@@ -0,0 +1,19 @@
+﻿using MVCShop.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCShop.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        public async Task<ActionResult> Index()
+        {
+            var products = db.Products.Where(p => (p.Deleted == false && p.Visible == true && p.Discount != 0)).Include(p => p.Category);
+            return View(await products.ToListAsync());
+        }
+    }
+}
