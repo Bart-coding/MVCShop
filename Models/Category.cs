06f13d3cf3d3e5bdbc3c36f31043d08286887f12
Mateusz Rytwiński <mateuszrytwinski@onet.pl>
2021-06-09 18:31:11 +0200

    edited CRUD for Categories

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCShop.Models
@@ -6,9 +7,14 @@ namespace MVCShop.Models
     public class Category
     {
         public int CategoryID { get; set; }
+
+        [Display(Name="Nazwa")]
         public string Name { get; set; }
+
+        [Display(Name = "Widoczna")]
         public bool Visible { get; set; }
 
+        [Display(Name = "Kategoria nadrzędna")]
         [ForeignKey("CategoryType")]
         public int? CategoryTypeID { get; set; }
         public virtual Category CategoryType { get; set; }
