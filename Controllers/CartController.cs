a9e0c1900cb2c9eaa15af5f4bb9c20d7cae5350c
Bart-coding <bartekpoczta54@gmail.com>
2021-06-16 17:23:16 +0200

    Implemented shopping cart (to debug)

---
+++ Controllers/CartController.cs
@@ -0,0 +1,128 @@
+﻿using MVCShop.DTO;
+using MVCShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCShop.Controllers
+{
+    [Authorize(Roles = "user")] //
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Cart
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            if (Session["cart"] == null)
+            {
+                return View();
+            }
+
+
+            List<ProductsCountDto> cartProducts = (List<ProductsCountDto>)Session["cart"];
+
+            List<Product> listToShow = new List<Product>();
+            List<int> productsQuantities = new List<int>();
+            double priceSum = 0;
+            Product tempProduct;
+            int tempProductID, tempQuantity;
+            for (int i = 0; i < cartProducts.Count; i++)
+            {
+                tempProductID = cartProducts[i].ProductID;
+                tempProduct = await db.Products.FindAsync(tempProductID);
+                listToShow.Add(tempProduct);
+                tempQuantity = cartProducts[i].Count;
+                productsQuantities.Add(tempQuantity);
+                priceSum += (double)(tempProduct.Price * tempQuantity);
+
+            }
+
+            ViewBag.productsQuantities = productsQuantities;
+            ViewBag.priceSum = priceSum;
+
+            return View(listToShow);
+        }
+
+        public ActionResult Add(int? productId)
+        {
+            System.Diagnostics.Debug.WriteLine(productId==null?"siema":"A");
+            if (productId != null)
+            {
+                List<ProductsCountDto> cartProducts;
+                if (Session["cart"] == null)
+                {
+                    cartProducts = new List<ProductsCountDto>();
+                }
+
+                else
+                {
+                    cartProducts = (List<ProductsCountDto>)Session["cart"];
+                    int productIndex = cartProducts.FindIndex(p => p.ProductID == productId);
+                    if (productIndex > -1)
+                    {
+                        var product = cartProducts[productIndex];
+                        product.Count++;
+                        cartProducts[productIndex] = product;
+                        return RedirectToAction("Index");
+                    }
+
+                }
+
+                cartProducts.Add(new ProductsCountDto { ProductID = (int)productId, Count = 1 });
+
+                Session["cart"] = cartProducts;
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Delete(int? productId)
+        {
+            if (productId == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            List<ProductsCountDto> cartProducts;
+            if (Session["cart"] == null)
+                return View();//
+
+            cartProducts = (List<ProductsCountDto>)Session["cart"];
+
+            int productIndex = cartProducts.FindIndex(p => p.ProductID == productId);
+            if (productIndex > -1)
+            {
+                var product = cartProducts[productIndex];
+                if (product.Count > 1)
+                {
+                    product.Count--;
+                    cartProducts[productIndex] = product;
+                }
+                else
+                    cartProducts.RemoveAt(productIndex);
+
+            }
+
+            Session["cart"] = cartProducts;
+
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Product product = await db.Products.FindAsync(id);
+            if (product == null || product.Deleted || !product.Visible)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Controllers/ShowProductsController.cs
@@ -0,0 +1,73 @@
+﻿using MVCShop.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCShop.Controllers
+{
+    public class ShowProductsController : Controller
+    {
+        // GET: ShowProducts
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private IdentityManager im = new IdentityManager();
+
+        [Authorize(Roles ="user")]
+        public async Task<ActionResult> Index(int? page)
+        {
+            var user = im.GetCurentUser();
+            bool netto = user.Netto; //
+            int discount = user.PersonalDiscount; //
+
+            var productsToShow = db.Products.Where(p => p.Deleted == false && p.Visible == true);
+
+            int total = productsToShow.Count();
+
+            var elemsPerSize = 10;
+            // wyświetlanie liczby produktów ustawionych w profilu, jeśli użytkownik zalogowany
+            if (User.Identity.IsAuthenticated)
+            {
+                elemsPerSize = user.ProductsPerPage;
+            }
+            var skip = 0;
+            if (page != null)
+            {
+                skip = (int)(elemsPerSize * (page - 1));
+            }
+            var canPage = skip < total;
+
+            if (!canPage)
+            {
+                return View();
+            }
+            else
+            {
+                var products = productsToShow.OrderBy(x => x.CategoryID)
+                                    .ThenBy(x => x.Price)
+                                    .Skip(skip)
+                                    .Take(elemsPerSize)
+                                    .Include(p => p.Category);
+
+                ViewBag.CurrentPage = page ?? 1;
+                ViewBag.NumberOfPages = Math.Ceiling(total / (double)elemsPerSize);
+
+                return View(await products.ToListAsync());
+            }
+        }
+
+        public ActionResult Buy (int? productId)
+        {   //return RedirectToAction("AddToCart", "Cart", {productId});
+            //RedirectToAction("AddToCart", new RouteValueDictionary(
+            // new { controller = "Cart", action = "AddToCart", productId = 2 }));
+            if (productId != null)
+                return RedirectToAction("Add", "Cart", new { productId });
+            else
+               return RedirectToAction("Index");
+        }
+        
+    }
+}
